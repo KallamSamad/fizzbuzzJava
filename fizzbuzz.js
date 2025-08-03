64fe9e93e95c53c53a96cb57312c894482dbbe1a
KallamSamad <kallamsamad@gmail.com>
2025-08-03 15:25:48 +0100

    Completed fizzbuzz program

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,19 @@
-# Fizz buzz
+const enterNum =prompt("Enter your number: ")
+for (let x=1; x <= enterNum; x++) {
+    console.log(x)
+
+
+    if (x % 3==0){
+        console.log("fizz")
+
+    }
+    else if(x % 3==0) {
+            console.log("buzz")
+        }
+    else if ((x % 3==0) && (x%5==0)) {
+        console.log("Fizzbuzz")
+    }
+
+}
+
+
